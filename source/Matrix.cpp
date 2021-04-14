6d529cd75cd37eacb5158d8f7102326967157921
JiayunLi-3E8 <2936221683@qq.com>
2021-04-14 12:29:11 +0800

    finished matrix and start square_matrix

--- source/Matrix.cpp
+++ source/Matrix.cpp
@@ -6,10 +6,18 @@ using namespace std;
 
 namespace JoY
 {
-	Matrix::Matrix(int m, int n)
+	bool MN::operator==(const MN &mn)
 	{
-		mn.m = m;
-		mn.n = n;
+		return mn.m == m && mn.n == n;
+	}
+
+	bool MN::operator!=(const MN &mn)
+	{
+		return !(*this == mn);
+	}
+
+	void Matrix::New()
+	{
 		A = new Fraction *[mn.m];
 		for (int i = 0; i < mn.m; i++)
 		{
@@ -17,24 +25,39 @@ namespace JoY
 		}
 	}
 
-	Matrix::~Matrix()
+	void Matrix::Del()
 	{
 		for (int i = 0; i < mn.m; i++)
 		{
 			delete[] A[i];
 		}
 		delete[] A;
-		// std::cout << "~" << this << std::endl;
+	}
+
+	void Matrix::Renew(int m, int n)
+	{
+		Del();
+		mn.m = m;
+		mn.n = n;
+		New();
+	}
+
+	Matrix::Matrix(int m, int n)
+	{
+		mn.m = m;
+		mn.n = n;
+		New();
+	}
+
+	Matrix::~Matrix()
+	{
+		Del();
+		std::cout << "~" << this << std::endl;
 	}
 
 	MN Matrix::getMN()
 	{
 		return mn;
-	}
-
-	Fraction **Matrix::useA()
-	{
-		return A;
 	}
 
 	Matrix Matrix::getAT()
@@ -143,5 +166,181 @@ namespace JoY
 		}
 
 		return aSimplestLine;
+	}
+
+	int Matrix::r()
+	{
+		Matrix asl(getSimplestLine());
+		int m = 0;
+		while (m < asl.mn.m)
+		{
+			int n = 0;
+			while (n < asl.mn.n)
+			{
+				if (asl.A[m][n] != 0)
+				{
+					break;
+				}
+				n++;
+			}
+			if (n >= asl.mn.n)
+			{
+				break;
+			}
+			m++;
+		}
+		return m;
+	}
+
+	const Matrix &Matrix::operator=(const Matrix &a)
+	{
+		this->Renew(a.mn.m, a.mn.n);
+		for (int m = 0; m < mn.m; m++)
+		{
+			for (int n = 0; n < mn.n; n++)
+			{
+				A[m][n] = a.A[m][n];
+			}
+		}
+		return a;
+	}
+
+	Matrix Matrix::operator+(Matrix &a)
+	{
+		Matrix newA(mn.m, mn.n);
+		if (a.mn == mn)
+		{
+			for (int m = 0; m < mn.m; m++)
+			{
+				for (int n = 0; n < mn.n; n++)
+				{
+					newA.A[m][n] = A[m][n] + a.A[m][n];
+				}
+			}
+		}
+		else
+		{
+			invalid_argument e("The 'm,n' of the two matrices are different and cannot be added.");
+			throw e;
+		}
+		return newA;
+	}
+
+	Matrix Matrix::operator-(Matrix &a)
+	{
+		Matrix newA(mn.m, mn.n);
+		if (a.mn == mn)
+		{
+			for (int m = 0; m < mn.m; m++)
+			{
+				for (int n = 0; n < mn.n; n++)
+				{
+					newA.A[m][n] = A[m][n] - a.A[m][n];
+				}
+			}
+		}
+		else
+		{
+			invalid_argument e("The 'm,n' of the two matrices are different and cannot be subtracted.");
+			throw e;
+		}
+		return newA;
+	}
+
+	void Matrix::operator+=(Matrix &a)
+	{
+		if (a.mn == mn)
+		{
+			for (int m = 0; m < mn.m; m++)
+			{
+				for (int n = 0; n < mn.n; n++)
+				{
+					A[m][n] += a.A[m][n];
+				}
+			}
+		}
+		else
+		{
+			invalid_argument e("The 'm,n' of the two matrices are different and cannot be added.");
+			throw e;
+		}
+	}
+
+	void Matrix::operator-=(Matrix &a)
+	{
+		if (a.mn == mn)
+		{
+			for (int m = 0; m < mn.m; m++)
+			{
+				for (int n = 0; n < mn.n; n++)
+				{
+					A[m][n] -= a.A[m][n];
+				}
+			}
+		}
+		else
+		{
+			invalid_argument e("The 'm,n' of the two matrices are different and cannot be subtracted.");
+			throw e;
+		}
+	}
+
+	Matrix Matrix::operator*(const Matrix &a)
+	{
+		Matrix newA(mn.m, a.mn.n);
+		if (mn.n == a.mn.m)
+		{
+			for (int m = 0; m < mn.m; m++)
+			{
+				for (int n = 0; n < a.mn.n; n++)
+				{
+					Fraction sum;
+					for (int i = 0; i < mn.n; i++)
+					{
+						sum += A[m][i] * a.A[i][n];
+					}
+					newA.A[m][n] = sum;
+				}
+			}
+		}
+		else
+		{
+			invalid_argument e("The 'n' of the left matrix is different from the 'm' of the right and cannot be multiplied.");
+			throw e;
+		}
+		return newA;
+	}
+
+	istream &operator>>(istream &input, Matrix &a)
+	{
+		int m, n;
+		cout << endl
+			 << "Input m,n of matrix(Separate by space):" << endl;
+		input >> m >> n;
+		a.Renew(m, n);
+		cout << "----------------Input items of matrix----------------" << endl;
+		for (m = 0; m < a.mn.m; m++)
+		{
+			for (n = 0; n < a.mn.n; n++)
+			{
+				input >> a.A[m][n];
+			}
+			cout << endl;
+		}
+		return input;
+	}
+
+	ostream &operator<<(ostream &output, Matrix &a)
+	{
+		for (int m = 0; m < a.mn.m; m++)
+		{
+			output << endl;
+			for (int n = 0; n < a.mn.n; n++)
+			{
+				output << a.A[m][n] << "\t";
+			}
+			output << endl;
+		}
+		return output;
 	}
 }
--- source/main.cpp
+++ source/main.cpp
@@ -6,65 +6,31 @@ using namespace JoY;
 
 int main()
 {
-	int m, n;
+	cout << "----------------> Matrix calculation <----------------" << endl;
 
-	cout << "Matrix calculation, please separate input with spaces." << endl
-		 << "Input the m,n of matrix [a]:" << endl
-		 << endl;
-	cin >> m >> n;
-	Matrix a(m, n);
+	SquareMatrix a;
+	cout << a;
 
-	cout << endl
-		 << "----------------Input items of [a]-----------------" << endl;
-	for (m = 0; m < a.getMN().m; m++)
-	{
-		for (n = 0; n < a.getMN().n; n++)
-		{
-			cin >> a.useA()[m][n];
-		}
-		cout << endl;
-	}
+	// int m, n;
+	// Matrix a, b;
 
-	cout << "m,n of [a]: " << a.getMN().m << "," << a.getMN().n << endl
-		 << "[a]:"
-		 << endl;
-	for (m = 0; m < a.getMN().m; m++)
-	{
-		for (n = 0; n < a.getMN().n; n++)
-		{
-			cout << a.useA()[m][n] << "\t";
-		}
-		cout << endl
-			 << endl;
-	}
+	// cin >> a >> b;
+	// Matrix c(a * b);
+	// cout << c;
 
-	Matrix aT(a.getAT());
-	cout << "m,n of [aT]: " << aT.getMN().m << "," << aT.getMN().n << endl
-		 << "[aT]:"
-		 << endl;
-	for (m = 0; m < aT.getMN().m; m++)
-	{
-		for (n = 0; n < aT.getMN().n; n++)
-		{
-			cout << aT.useA()[m][n] << "\t";
-		}
-		cout << endl
-			 << endl;
-	}
+	// cin >> a;
+	// cout
+	// 	<< "m,n of [a]: " << a.getMN().m << "," << a.getMN().n << endl
+	// 	<< "[a]:" << a << "r(a)=" << a.r() << endl
+	// 	<< endl;
 
-	Matrix aSL(a.getSimplestLine());
-	cout << "m,n of [aSL]: " << aSL.getMN().m << "," << aSL.getMN().n << endl
-		 << "[aSL]:"
-		 << endl;
-	for (m = 0; m < aSL.getMN().m; m++)
-	{
-		for (n = 0; n < aSL.getMN().n; n++)
-		{
-			cout << aSL.useA()[m][n] << "\t";
-		}
-		cout << endl
-			 << endl;
-	}
+	// Matrix aT(a.getAT());
+	// cout << "m,n of [aT]: " << aT.getMN().m << "," << aT.getMN().n << endl
+	// 	 << "[aT]:" << aT << endl;
+
+	// Matrix aSL(a.getSimplestLine());
+	// cout << "m,n of [aSL]: " << aSL.getMN().m << "," << aSL.getMN().n << endl
+	// 	 << "[aSL]:" << aSL << endl;
 
 	return 0;
 }
